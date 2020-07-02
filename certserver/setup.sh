934b06b7164a83d8d80ac47509dfe7621ce75f41
Tom Kelley <distortedsignal@gmail.com>
2020-07-02 09:14:23 -0700

    Add the Dockerfile, setup script to the certserver dir.

--- certserver/setup.sh
+++ certserver/setup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+CONTAINER_TAG="distortedsignal/cert_server"
+VERSION_MAJOR="1"
+VERSION_MINOR="0"
+VERSION_BUGFIX="0"
+
 git clone git@github.com:jsha/minica.git 
 
 cd minica
@@ -8,4 +13,13 @@ GO111MODULE="on" go build
 
 cd ..
 
-docker build -t distortedsignal/cert_server:1.0.0 -t distortedsignal/cert_server:1.0 -t distortedsignal/cert_server:1 -t distortedsignal/cert_server:latest .
+docker rmi "$CONTAINER_TAG":"$VERSION_MAJOR"."$VERSION_MINOR"."$VERSION_BUGFIX" \
+           "$CONTAINER_TAG":"$VERSION_MAJOR"."$VERSION_MINOR" \
+           "$CONTAINER_TAG":"$VERSION_MAJOR" \
+           "$CONTAINER_TAG":latest
+
+docker build -t "$CONTAINER_TAG":"$VERSION_MAJOR"."$VERSION_MINOR"."$VERSION_BUGFIX" \
+             -t "$CONTAINER_TAG":"$VERSION_MAJOR"."$VERSION_MINOR" \
+             -t "$CONTAINER_TAG":"$VERSION_MAJOR" \
+             -t "$CONTAINER_TAG":latest \
+             .
