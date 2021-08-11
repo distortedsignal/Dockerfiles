c70709f23b475a57f0d518b9388b470bdca38ff7
Tom Kelley <distortedsignal@gmail.com>
2021-08-11 16:00:11 -0700

    Ok, this kinda works now.

--- opencve/entrypoint.sh
+++ opencve/entrypoint.sh
@@ -13,7 +13,7 @@ $RUN_AS_PG "pg_ctl start -D /home/postgres"
 $RUN_AS_PG "psql -c \"CREATE DATABASE opencve\" template1"
 
 $RUN_AS_PG "opencve init"
-$RUN_AS_PG "sed -e 's/^database_uri.*$/database_uri = postgresql:\/\/localhost:5432\/opencve/g' $OPENCVE_CFG_LOCATION | sed -e 's/^server_name.*$/server_name = 0.0.0.0:8000/g' >> $OPENCVE_CFG_LOCATION.final"
+$RUN_AS_PG "sed -e 's/^database_uri.*$/database_uri = postgresql:\/\/localhost:5432\/opencve/g' $OPENCVE_CFG_LOCATION | sed -e 's/^server_name.*$/server_name = localhost:9000/g' >> $OPENCVE_CFG_LOCATION.final"
 $RUN_AS_PG "mv $OPENCVE_CFG_LOCATION.final $OPENCVE_CFG_LOCATION"
 $RUN_AS_PG "opencve upgrade-db"
 $RUN_AS_PG "redis-server &"
@@ -23,30 +23,6 @@ $RUN_AS_PG "opencve celery beat -l INFO &"
 
 $RUN_AS_PG "opencve create-user --admin --password admin admin admin@example.com"
 
-sleep 10
-
-echo
-echo
-echo "Opencve cfg:"
-cat $OPENCVE_CFG_LOCATION
-echo
-echo
-
-echo "Opencve version from opencve --version:"
-opencve --version
-echo
-echo
-echo "Python version:"
-python --version
-echo
-echo "Python3 version:"
-python3 --version
-
 $RUN_AS_PG "opencve import-data --confirm"
 
-echo "Running webserver"
-$RUN_AS_PG "opencve webserver &"
-
-sh
-
-
+$RUN_AS_PG "opencve webserver -b 0.0.0.0:9000"
